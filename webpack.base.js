216bab4f3efb398a1c7e8b82b775f52b87f6a0ab
Victor Freitas <victorfreitas1@gmail.com>
2019-06-26 10:27:38 -0300

    Removed old modules and add lint to webpack

--- webpack.base.js
+++ webpack.base.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const stylish = require('eslint/lib/cli-engine/formatters/stylish')
 
 const isDev = process.env.NODE_ENV !== 'production'
 const baseConfig = {
@@ -12,6 +13,17 @@ const baseConfig = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            formatter: stylish,
+          },
+        },
       },
     ],
   },
